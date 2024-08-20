31a25260746a29df7d8d2c738bf563be5ecac6af
Gise-Salazar <sa24002@esfe.agape.edu.sv>
2024-08-20 08:35:44 -0600

    AVANCE 3

--- ESFE.SysMatriculaEstudiantes.BL/EstudianteBL.cs
+++ ESFE.SysMatriculaEstudiantes.BL/EstudianteBL.cs
@@ -22,43 +22,9 @@ namespace ESFE.SysMatriculaEstudiantes.BL
             return estudianteDAL.InsertarEstudiante(estudiante);
         }
 
-        //otro metodo Seleccionar todo
-        public List<Estudiante> ObtenerEstudiante()
-        {
-            List<Estudiante> _marcas = new List<Estudiante>();
-
-
-            //la lista es para tener todos los valores necesitamos que devuelva todo lo que esta en la tabla
-            using (IDbConnection _conn = BDComun.ObtenerConexion())
-            {
-                _conn.Open();
-                SqlCommand _command = new SqlCommand("SELECT * FROM Estudiante", _conn as SqlConnection);
-                SqlDataReader _reader = _command.ExecuteReader();//esto se lo se usa con el select
-
-                while (_reader.Read())
-                {
-                    Estudiante estudiante = new Estudiante
-                    {
-
-                        //esto es un casteo esto pasar parametros al tipo de datos que va a leer
-                        Id = (int)_reader.GetSqlInt32(0),
-                        Nombres = _reader.GetString(1),
-                        Direccion = _reader.GetString(2),
-                        Telefono = (int)_reader.GetSqlInt32(3),
-                        Apellidos = _reader.GetString(4)
-
-
-                    };
-
-                    estudiante.Add(estudiante);
-                }
-
-                _reader.Close();
-                _conn.Close();
-                return estudiante;
-            }
-        }
+      
     }
 }
+
 
 
